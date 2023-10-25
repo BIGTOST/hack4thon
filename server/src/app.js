c822254e54441b9a4075b40db89e8cd21b7c7fa0
(Alex)andre Cardoso <100371225+BIGTOST@users.noreply.github.com>
2023-10-25 11:31:34 +0100

    update

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const route = express.Router()
+const path = require('path');
+
+
+
+const ai = require('./aiBot.js');
+
+app.set('port', process.env.PORT||3000);
+app.use(cors());
+app.use(express.static('public'))
+app.use(express.json());
+
+app.use('/',ai.botaAi)
+
+route.get('/mensagem', ai.botaAi);
+
+app.listen(app.get('port'),()=>{
+    console.log('Start server on port' + app.get('port'));
+})
